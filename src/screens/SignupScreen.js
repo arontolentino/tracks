8a02514ca9c092c8b26408c902ca5429cfa4fc67
Aron Tolentino <arontolentino@outlook.com>
2020-04-15 15:08:50 -0400

    Implement sign in using locally stored token

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
 
@@ -8,7 +8,13 @@ import AuthForm from '../components/AuthForm';
 import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = () => {
-	const { state, signup, clearErrorMessage } = useContext(AuthContext);
+	const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(
+		AuthContext
+	);
+
+	useEffect(() => {
+		tryLocalSignin();
+	}, []);
 
 	return (
 		<View style={styles.container}>
