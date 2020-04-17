bdc1c92cef3166446e78637dd6a67b58296e7ddf
Aron Tolentino <arontolentino@outlook.com>
2020-04-16 21:58:12 -0400

    Ask location permissions and detect current user location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,45 @@
-import React from 'react';
+// import '../_mockLocation';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import {
+	requestPermissionsAsync,
+	watchPositionAsync,
+	Accuracy,
+} from 'expo-location';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+	const [err, setErr] = useState(null);
+
+	const startWatching = async () => {
+		try {
+			await requestPermissionsAsync();
+			await watchPositionAsync(
+				{
+					accuracy: Accuracy.BestForNavigation,
+					timeInterval: 1000,
+					distanceInterval: 10,
+				},
+				(location) => {
+					console.log(location);
+				}
+			);
+		} catch (e) {
+			setErr(e);
+		}
+	};
+
+	useEffect(() => {
+		startWatching();
+	}, []);
+
 	return (
 		<SafeAreaView>
 			<Text h2>Create a Track</Text>
 			<Map />
+			{err ? <Text>Please enable location services</Text> : null}
 		</SafeAreaView>
 	);
 };
